06c661da4185a75f74d12e78072b712b0db5306b
T-Wesst <code.daddy17@gmail.com>
2019-09-11 15:49:27 -0700

    add login signup logic

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,14 @@ import Logout from './pages/Logout';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import axios from 'axios';
 export default class App extends Component {
   state = {
     message: null,
     username: '',
     password: '',
+    email: '',
     error: null,
     isAuth: false
   };
@@ -19,7 +21,25 @@ export default class App extends Component {
   handleOnChange = event => {
     this.setState({ [event.target.id]: event.target.value });
   };
-  handleLogin = () => {
+  handleSignUp = event => {
+    event.preventDefault();
+    const { username, email, password } = this.state;
+    axios
+      .post('/api/users/signup', { username, password, email })
+      .then(response => {
+        let isAuth = response.data.status === 'OK' ? true : false;
+        this.setState({ isAuth, message: response.data.message, error: null });
+        alert(this.state.message);
+      })
+      .catch(error => {
+        if (error) {
+          this.setState({ error, message: error.response.data.message });
+          alert(this.state.message);
+        }
+      });
+  };
+  handleLogin = event => {
+    event.preventDefault();
     const { username, password } = this.state;
     axios
       .post('/api/users/login', { username, password })
@@ -34,10 +54,6 @@ export default class App extends Component {
         alert(this.state.message);
       });
   };
-  handleSubmit = event => {
-    event.preventDefault();
-    this.handleLogin();
-  };
 
   render() {
     return (
@@ -45,22 +61,18 @@ export default class App extends Component {
         <Router>
           {this.state.isAuth ? <Header message={this.state.message} /> : ''}
           <Switch>
-            <Route
-              exact
-              path="/"
-              render={() => (
-                <Landing
-                  handleSubmit={this.handleSubmit}
-                  handleChange={this.handleOnChange}
-                />
-              )}
-            />
-            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/" render={() => (
+                <Landing handleSubmit={this.handleLogin} handleChange={this.handleOnChange}/>
+              )}/>
+            <Route exact path="/signup" render={() => (
+                <SignUp handleChange={this.handleOnChange} handleSubmit={this.handleSignUp}/>
+              )}/>
             <Route exact path="/dashboard" component={Dashboard} />
             <Route exact path="/logout" component={Logout} />
             <Route component={NotFound} />
           </Switch>
         </Router>
+        <Footer />
       </>
     );
   }
